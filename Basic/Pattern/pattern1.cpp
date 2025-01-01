68e2b5499ff5bb286bf7b65975b23830bf4d6d41
Udaykumar725 <udaykumar77725@gmail.com>
2025-01-01 11:07:10 +0530

    pattern program added

---
+++ Basic/Pattern/pattern1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<< "Enter no of rows:";
+    cin>>n;
+
+    for(int i=0; i<5; i++){
+        for(int i=0; i<5; i++){
+          cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
